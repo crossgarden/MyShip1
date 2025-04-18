0b4c367d76e5fd080f9e7f4185740b0a80386423
GeunhoBae <paegh0811@gmail.com>
2025-04-18 19:52:31 +0900

    운석 프리팹, 하강, 좌우반전

--- MyShipPJ/Assets/Games/Meteor/Scripts/Meteor_Obstacle.cs
+++ MyShipPJ/Assets/Games/Meteor/Scripts/Meteor_Obstacle.cs
@@ -7,6 +7,26 @@ public class Meteor_Obstacle : MonoBehaviour
 
     void Update()
     {
-        
+        Move();
+        FlipX();
+    }
+
+    void Move() //운석,코인 낙하
+    {
+        Vector3 movement = Vector3.down * Time.deltaTime * Meteor_Manager.instance.speed;
+        transform.position += movement;
+
+        if (transform.position.y < -8)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void FlipX()
+    {
+        // 현재 localScale의 x 값을 반전시킴
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
     }
 }
